ceb09c5619ad038e79b63993d68ffe43a751a1c3
Maximilian Hemmrich <maximilian.hemmrich@gmail.com>
2015-01-03 20:47:20 -0500

    Moved main logic to function

--- iconCreator.sh
+++ iconCreator.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-DimensionLabels=(mdpi,hdpi,xhdpi,xxhdpi,xxxhdpi)
+DimensionLabels=("mdpi" "hdpi" "xhdpi" "xxhdpi" "xxxhdpi" "GooglePlay")
 Launcher=(48 72 96 144 192 512)
-ActionBar=(32,48,64,96,128)
-Notification=(24,36,48,72,96)
+ActionBar=(32 48 64 96 128)
+Notification=(24 36 48 72 96)
 
 #argument 1 ($1) should be file name, argument 2 ($2) should be mode
 usage="Usage: ./iconCreator.sh <filename of PNG image> <mode: launcher, actionbar, or notification>"
@@ -18,7 +18,28 @@ function getNewFilename {
     newFilename+='.png'
 }
 
+function createImages {
+    #first argument is array size, second is PNG name, remainder are array elements
+    numElts="${1}" 
+    filename="${2}"
+    shift
+    shift
+    array=("${@}")
 
+    for i in "${!array[@]}"
+    do
+        dimension=${array[$i]}
+        getNewFilename $filename $dimension
+        cp $filename $newFilename
+
+        #use sips to resize image to suppress output
+        {sips -z $dimension $dimension $newFilename} &> /dev/null
+
+        label=${DimensionLabel[$i]}
+        echo "Created $label image $newFilename"
+    done
+
+}
 
 #print usage and quit if wrong number of arguments were passed
 [ $# -ne 2 ] && echo $usage && exit 1
@@ -27,35 +48,12 @@ function getNewFilename {
 [ ! -f $1 ] && echo "File not found!" && exit 1
 [ ${1: -4} != ".png" ] && echo "Not a valid PNG file!" && exit 1
 
-#check if mode is valid
-if [[ $2 != "launcher" ]] && [[ $2 != "actionbar" ]] && [[ $2 != "notification" ]]
-then
-    echo 'Invalid mode argument. Argument should be "launcher", "actionbar", or "notification"'
-    exit 1
-fi
 
-if [ $2 == "launcher" ]
-then
-    echo "Launcher"
-    echo "filename: $1" 
-    for size in "${Launcher[@]}"
-    do
-        echo "$size"
-        getNewFilename $1 $size
-        echo $newFilename
-        #cp $1
-    done
+#main logic
+[ $2 == "launcher" ]     && createImages ${#Launcher[@]} $1 "${Launcher[@]}"         && exit 0
+[ $2 == "actionbar" ]    && createImages ${#ActionBar[@]} $1 "${ActionBar[@]}"       && exit 0
+[ $2 == "notification" ] && createImages ${#Notification[@]} $1 "${Notification[@]}" && exit 0
 
-
-
-elif [ $2 == "actionbar" ]
-then
-    echo "ActionBar"
-elif [ $2 == "notification" ]
-then
-    echo "Notification"
-else
-    echo 'Invalid mode argument. Argument should be "launcher", "actionbar", or "notification".'
-    exit 1
-fi
+echo 'Invalid mode argument. Argument should be "launcher", "actionbar", or "notification".'
+exit 1
 
